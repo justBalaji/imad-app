868009ffd753f1ea23c1b5ef51250afebcc0f937
justBalaji <30663178+justBalaji@users.noreply.github.com>
2017-08-12 18:45:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article -one', function (req, res) {
+    res.sendfile("article one will be served here");
+});
+
+app.get('/article-two', function (req,res) {
+    res.sendfile("article two will be served here");
+});
+
+app.get('/article-three', function (req,res) {
+    res.sendfile("article three will be served here");
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
